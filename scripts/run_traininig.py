ba5c9969aeb045cc1bd5d32703621c95062e6358
Laraib Ahmad <112575218+Laraib17@users.noreply.github.com>
2025-11-11 23:33:50 +0530

    feat:added data

---
+++ scripts/run_traininig.py
@@ -0,0 +1,11 @@
+def convert_to_uppercase(input_string):
+    """
+    Converts the given input string to uppercase.
+
+    Parameters:
+    input_string (str): The string to be converted.
+
+    Returns:
+    str: The uppercase version of the input string.
+    """
+    return input_string.upper()
